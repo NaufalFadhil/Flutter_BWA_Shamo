49c7ef4d0f08685e1e6a1c09d3157544654435bf
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-06-02 23:22:40 +0700

    feat(home): create header for name, username, and picture

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -6,9 +6,56 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryBackgroundColor,
-      body: Center(child: Text('Home Screen', style: primaryTextStyle,)),
+
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Naufal Fadhil',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@naufalfadhil',
+                    style: tertiaryTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/avatar_default.png'
+                  )
+                )
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return ListView(
+      children: [
+        header()
+      ],
     );
   }
 }
